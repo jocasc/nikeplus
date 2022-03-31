8080662663ae5b70e89441863739b175da339385
jocasc <36902654+jocasc@users.noreply.github.com>
2022-03-31 21:43:47 +0100

    mostrar todos os campos

--- corridas/serializer.py
+++ corridas/serializer.py
@@ -5,8 +5,7 @@ from corridas.models import Running, Distance
 class RunningSerializer(serializers.ModelSerializer):
     class Meta:
         model = Running
-        fields = ['name', 'description', 'total_distance', 'running_start', 'running_end',
-                  'elapsed_time', 'calories', 'cadence', 'elevation_gain', 'elevation_loss']
+        fields = '__all__'
 
 
 class DistanceSerializer(serializers.ModelSerializer):
